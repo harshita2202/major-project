0f1a9c55158b61214bb2a874dc1c24ec16355060
harshita2202 <Khandelwalharshita2205@gmail.com>
2026-09-18 19:12:28 +0530

    first commit

---
+++ backend/src/main/java/com/proctoring/proctoring_backend/ProctoringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proctoring.proctoring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProctoringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProctoringBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/proctoring/proctoring_backend/ProctoringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proctoring.proctoring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProctoringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
